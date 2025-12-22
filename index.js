1404fb9d343661929a9c27377b8afedd731fc357
ahmadhafifihamdan <ahmadhafifihamdan@gmail.com>
2025-12-22 17:47:54 +0800

    rename server to index. installed and tested express

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 
 const app = express();
+
+//  View Engine
+app.set("view engine", "ejs");
+
+
+// Middleware
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static("public"));
+
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
