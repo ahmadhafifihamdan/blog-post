17325027893e476838254f275bc4c9db8261013a
ahmadhafifihamdan <ahmadhafifihamdan@gmail.com>
2025-12-23 12:06:36 +0800

    feat: Add Firebase SDK config to server

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
-
+const { PORT } = require("./config/env.config");
 const app = express();
 
 //  View Engine
 app.set("view engine", "ejs");
-
 
 // Middleware
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.static("public"));
-
-const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
     res.send("Server is running")
