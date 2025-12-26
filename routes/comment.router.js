87c341fd3984b65c3cdac08d8bbe20b60efef0df
ahmadhafifihamdan <ahmadhafifihamdan@gmail.com>
2025-12-26 19:41:52 +0800

    chore: remove unused routers after refactor

--- routes/comment.router.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const { protect } = require("../middleware/auth.middleware");
-const { addComment } = require("../controllers/comment.controller");
-
-const router = Router();
-
-router.route("/comment").post(protect, addComment);
-
-module.exports = router;
--- routes/like.router.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const { protect } = require("../middleware/auth.middleware");
-const { toggleLikeHandler } = require("../controllers/like.controller");
-
-const router = Router();
-
-router.route("/like").post(protect, toggleLikeHandler);
-
-module.exports = router;
